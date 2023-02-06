345d41b52ce50f0e59215c2f8a3c55dc239f48d1
roctbb <roctbb@gmail.com>
2023-02-06 21:22:52 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,5 +3,5 @@ sudo cp agents_netrika.conf /etc/supervisor/conf.d/
 sudo cp agents_netrika_nginx.conf /etc/nginx/sites-enabled/
 sudo supervisorctl update
 sudo systemctl restart nginx
-sudo certbot --nginx -d netrika.medsenger.ru
+sudo certbot --nginx -d netrika.ai.medsenger.ru
 touch config.py
